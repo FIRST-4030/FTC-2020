a42a0cc9b0930be27b96ec3777a401cc5fd10ca3
anders520 <anderschoy0604@gmail.com>
2021-02-24 16:31:33 -0800

    Added in variables and setPosition for wobbleGoalGrip

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/OdometryTestTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/OdometryTestTeleOp.java
@@ -16,6 +16,9 @@ public class OdometryTestTeleOp extends OpMode {
     private Robot robot = null;
     private ButtonHandler buttons;
 
+    //servo constants
+    private static final float WGGripOpen = 0.4f;
+    private static final float WGGripClosed = 0.6f;
 
     // other consts
     private static final float NORMAL_SPEED = 0.75f;
@@ -85,6 +88,11 @@ public class OdometryTestTeleOp extends OpMode {
         telemetry.addData("FL:", robot.wheels.getEncoder(MOTOR_SIDE.LEFT, MOTOR_END.FRONT));
         telemetry.addData("FR:", robot.wheels.getEncoder(MOTOR_SIDE.RIGHT, MOTOR_END.FRONT));
         robot.wobbleGoalArm.setPower(gamepad1.right_stick_y * 0.3f);
+        if (gamepad1.x) {
+            robot.wobbleGoalGrip.setPosition(WGGripClosed);
+        } else {
+            robot.wobbleGoalGrip.setPosition(WGGripOpen);
+        }
     }
 
 
