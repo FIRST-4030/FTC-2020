50257bf5c0a2eb612f64d1d2b959c6da97689eb7
anders520 <anderschoy0604@gmail.com>
2021-05-06 20:55:17 -0700

    Updated driveToPosition method in NewAuto again thrice

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/auto/NewAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/auto/NewAuto.java
@@ -212,7 +212,11 @@ public class NewAuto {
         if (dX < 0) {
             if (dY != 0) {
                 h = (Math.atan(dX / dY) * (180 / Math.PI)) % 360;
-                dH = h - H ;
+                if (dY > 0) {
+                    dH = h - H - 180;
+                } else if (dY < 0) {
+                    dH = h - H + 180;
+                }
             } else {
                 h = 180;
                 dH = h - H;
