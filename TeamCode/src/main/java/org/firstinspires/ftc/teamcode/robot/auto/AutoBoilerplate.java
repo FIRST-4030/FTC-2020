e18aa789b728d25fab380a04840f3dd7726dbaad
Robotics <meridanofthedomotopianrealm@gmail.com>
2021-05-07 19:50:42 -0700

    AutoBoilerplate is actually a boilerplate now

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/auto/AutoBoilerplate.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/auto/AutoBoilerplate.java
@@ -1,31 +1,20 @@
 package org.firstinspires.ftc.teamcode.robot.auto;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 
-import org.firstinspires.ftc.robotcore.external.navigation.VuforiaCurrentGame;
 import org.firstinspires.ftc.teamcode.buttons.BUTTON_TYPE;
 import org.firstinspires.ftc.teamcode.buttons.ButtonHandler;
 import org.firstinspires.ftc.teamcode.buttons.PAD_BUTTON;
 import org.firstinspires.ftc.teamcode.config.BOT;
-import org.firstinspires.ftc.teamcode.driveto.AutoDriver;
 import org.firstinspires.ftc.teamcode.field.Field;
 import org.firstinspires.ftc.teamcode.robot.Robot;
 import org.firstinspires.ftc.teamcode.robot.RobotConstants;
-import org.firstinspires.ftc.teamcode.robot.common.Common;
-import org.firstinspires.ftc.teamcode.utils.OrderedEnum;
-import org.firstinspires.ftc.teamcode.utils.OrderedEnumHelper;
-import org.firstinspires.ftc.teamcode.utils.Round;
 import org.firstinspires.ftc.teamcode.vuforia.VuforiaFTC;
 
 //@Disabled
 @com.qualcomm.robotcore.eventloop.opmode.Autonomous(name = "Auto", group = "Production")
 public class AutoBoilerplate extends LinearOpMode implements RobotConstants {
 
-    private int depot;
-    private RingStackTF ringDetector;
 
     // Devices and subsystems
     private Robot robot = null;
@@ -35,9 +24,6 @@ public class AutoBoilerplate extends LinearOpMode implements RobotConstants {
     // Runtime vars
     private boolean gameReady = false;
     private Field.AllianceColor color = Field.AllianceColor.BLUE;
-    private boolean stopByWall = true;
-
-    private PIDFCoefficients newPIDF;
     private int selectedPid = 0;
 
     public void runOpMode() {
@@ -55,31 +41,22 @@ public class AutoBoilerplate extends LinearOpMode implements RobotConstants {
             requestOpModeStop();
         }
 
-        // Init the camera system
+        // Init the camera system (if you need to)
         //vuforia.start();
         //vuforia.enableCapture();
 
+        //Replace BL and FR with whichever motors run auto.
         auto = new NewAuto("BL","FR", hardwareMap, robot.odometry);
-        ringDetector = new RingStackTF(hardwareMap, telemetry);
 
-        newPIDF = auto.getPIDFCoefficients();
-        robot.frontRaiseLower.setPosition(COLLECT_NO);
-        robot.backRaiseLower.setPosition(COLLECT_NO);
-        robot.wobbleGoalGrip.setPosition(CLAW_CLOSED);
-        sleep(350);
-        robot.wobbleGoalArm.setPosition(ARM_INIT);
         // Register buttons
         buttons = new ButtonHandler(robot);
-        buttons.register("SELECT_PID", gamepad1, PAD_BUTTON.y);
-        buttons.register("GO", gamepad1, PAD_BUTTON.a);
-        buttons.register("UP", gamepad1, PAD_BUTTON.dpad_up);
-        buttons.register("DOWN", gamepad1, PAD_BUTTON.dpad_down);
+        buttons.register("EXAMPLE_BUTTON", gamepad1, PAD_BUTTON.a, BUTTON_TYPE.SINGLE_PRESS);
 
-        robot.queue.setPosition(MAGAZINE_DOWN);
+        //REMEMBER TO INIT SERVOS HERE
+        //robot.servo.setPosition(servoInitPos);
 
         // Process driver input
-        userSettings();
-        while(!robot.gyro.isReady() && opModeIsActive()) {
+        while(!robot.gyro.isReady() && opModeIsActive() && !isStarted() && !isStopRequested()) {
             // Overall ready status
             gameReady = (robot.gyro.isReady());
             telemetry.addLine(gameReady ? "READY" : "NOT READY");
@@ -91,80 +68,29 @@ public class AutoBoilerplate extends LinearOpMode implements RobotConstants {
             telemetry.update();
         }
 
+        while(!isStopRequested() && !isStarted()) userSettings();
+
         waitForStart();
 
         // STUFF STARTS HAPPENING
+
         telemetry.clearAll();
-        depot = ringDetector.getTargetZone();
-        telemetry.addLine("Depot " + depot);
-        telemetry.update();
+
         // Log if we didn't exit init as expected
         if (!gameReady) {
             telemetry.log().add("! STARTED BEFORE READY !");
         }
+        telemetry.update();
 
-        //robot.vuforia.start();
-        //robot.vuforia.enableCapture();
+        if(!isStopRequested()) {
+            /*
+            AUTO ROUTINE HERE
+            */
 
-        auto.drive(52, 1);
-
-        robot.wobbleGoalArm.setPosition(ARM_IN);
-
-        auto.rotate(98, 1);
-
-        robot.queue.setPosition(MAGAZINE_UP);
-        robot.shooter.setVelocity(HIGH_SHOOTER_SPEED);
-        sleep(1500);
-        for(int i = 0; i < 3; i++){
-            robot.queueFlipper.setPosition(FLIPPER_SHOOT);
-            sleep(350);
-            robot.queueFlipper.setPosition(FLIPPER_IDLE);
-            sleep(500);
+            //auto.driveToPosition(24, 0, 1);
         }
-        robot.shooter.setVelocity(0);
-        robot.queue.setPosition(MAGAZINE_DOWN);
 
-
-        switch(depot){
-            case 0:
-                auto.rotate(-140, 1);
-                auto.drive(9, 1);
-                break;
-            case 1:
-                auto.rotate(-90, 1);
-                auto.drive(34, 1);
-                break;
-            case 2:
-                auto.rotate(-97, 1);
-                auto.drive(55, 1);
-                auto.rotate(-60, 1);
-                auto.drive(3, 1);
-                break;
-        }
-//woble goal ;)
-        robot.wobbleGoalArm.setPosition(ARM_OUT);
-        sleep(1000);
-        robot.wobbleGoalGrip.setPosition(CLAW_OPEN);
-        sleep(250);
-        robot.wobbleGoalArm.setPosition(ARM_IN);
-        sleep(500);
-
-        switch(depot){
-            case 0:
-                auto.drive(-20, 1);
-                auto.rotate(90, 1);
-                auto.drive(31, 1);
-                break;
-            case 1:
-                auto.drive(-14, 1);
-                break;
-            case 2:
-                auto.drive(-55, 1);
-                break;
-        }
-        //auto.driveToPosition(24, 0, 1);
-
-        }
+    }
 
 
     /**
@@ -180,35 +106,7 @@ public class AutoBoilerplate extends LinearOpMode implements RobotConstants {
         }
         telemetry.addData("Team Color", color.toString());
 
-        if (buttons.get("AWAY_FROM_WALL")) stopByWall = false;
-        if (buttons.get("TOWARDS_WALL")) stopByWall = true;
-        telemetry.addData("Stop by wall?", stopByWall);
 
-    }
-
-    /**
-     * Utility function to delegate AutoDriver to an external provider
-     * AutoDriver is handed back up to caller when the delegate sets done to true
-     *
-     * @param autoDriver AutoDriver to be delegated
-     * @return AutoDriver once delegate finishes
-     */
-    private AutoDriver delegateDriver(AutoDriver autoDriver) {
-        if (autoDriver.isDone()) {
-            autoDriver.done = false;
-        }
-
-        return autoDriver;
-    }
-
-    /**
-     * does what it says on the tin
-     *
-     * @param inches inches
-     * @return those inches but in millimeters
-     */
-    private int InchesToMM(float inches) {
-        return (int) (inches * 25.4);
     }
 
 }
